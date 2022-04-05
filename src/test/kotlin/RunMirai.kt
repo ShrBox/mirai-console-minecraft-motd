d54cf8f6e13a1304aeb8564c6331ddda242c32e9
ShrBox <shrbox@outlook.com>
2022-04-05 21:09:56 +0800

    Fix RunMirai.kt

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -5,16 +5,17 @@ import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+import top.bibk.minecraftmotd.Main
 
 suspend fun main() {
     MiraiConsoleTerminalLoader.startAsDaemon()
 
     //如果是Kotlin
-    PluginMain.load()
-    PluginMain.enable()
+    // PluginMain.load()
+    // PluginMain.enable()
     //如果是Java
-//    JavaPluginMain.INSTANCE.load()
-//    JavaPluginMain.INSTANCE.enable()
+    Main.INSTANCE.load()
+    Main.INSTANCE.enable()
 
     val bot = MiraiConsole.addBot(123456, "") {
         fileBasedDeviceInfo()
